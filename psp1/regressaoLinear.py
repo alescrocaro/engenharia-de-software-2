cfd8954bd88480f66d121bb0f91199718c2277e0
alescrocaro <alescrocaro@gmail.com>
2022-09-15 15:30:45 -0300

    refatoração e teste

--- psp1/regressaoLinear.py
+++ psp1/regressaoLinear.py
@@ -1,89 +1,117 @@
 import math
 
-def calculaDados(x, y, n, xk, xTotal = 0, xAvg = 0, xQuad = 0, yTotal = 0, yAvg = 0, yQuad = 0, somatoriaXY = 0):
+def calcula_xTotal(x, n):
+    # if (n <= 0): return None
+    # if (len(x) != n): return None
+
+    xTotal = 0
     for i in range(n):
         xTotal = xTotal + x[i]
-        yTotal = yTotal + y[i]
-        somatoriaXY = somatoriaXY + x[i]*y[i]
-        xQuad = xQuad + x[i]**2
-        yQuad = yQuad + y[i]**2
 
-    xAvg = xTotal/n
-    yAvg = yTotal/n
-    
-    b1 = (somatoriaXY - (n * xAvg * yAvg))/(xQuad - (n * (xAvg**2)))
-
-    b0 = yAvg - (b1*xAvg)
-
-    p = (n*somatoriaXY) - (xTotal*yTotal)
-    q = math.sqrt((n * xQuad - (math.pow(xTotal, 2))) * (n * yQuad - (math.pow(yTotal, 2))))
-
-    rxy = p/q
-    r2 = rxy*rxy
-    
-    yk = b0 + (b1*xk)
-
-    return b1, b0, rxy, r2, yk
+    return xTotal
 #end-function
 
+def calcula_yTotal(y, n):
+    # if (n <= 0): return None
+    # if (len(y) != n): return None
 
-########################
-print('CASO DE TESTE 1')
+    yTotal = 0
+    for i in range(n):
+        yTotal = yTotal + y[i]
 
+    return yTotal
+#end-function
+
+def calcula_somatoriaXY(x, y, n):
+    # if (n <= 0): return None
+    # if (len(x) != len(y)): return None
+    # if (len(x) != n): return None
+    # if (len(y) != n): return None
+
+    soma = 0
+    for i in range(n):
+        soma = soma + x[i]*y[i]
+
+    return soma
+#end-function
+
+def calcula_xQuad(x, n):
+    xQuad = 0
+    for i in range(n):
+        xQuad = xQuad + x[i]**2
+
+    return xQuad
+#end-function
+
+def calcula_yQuad(y, n):
+    yQuad = 0
+    for i in range(n):
+        yQuad = yQuad + y[i]**2
+
+    return yQuad
+#end-function
+
+def calcula_xAvg(xTotal, n):
+    return xTotal/n
+#end-function
+
+def calcula_yAvg(yTotal, n):
+    return yTotal/n
+#end-function
+
+def calcula_b1(x, y, n):
+    if(n <= 0): return None
+
+    xTotal = calcula_xTotal(x, n)
+    xAvg = calcula_xAvg(xTotal, n)
+
+    yTotal = calcula_yTotal(y, n)
+    yAvg = calcula_yAvg(yTotal, n)
+
+    xQuad = calcula_xQuad(x, n)
+    yQuad = calcula_yQuad(y, n)
+
+    somatoria_xy = calcula_somatoriaXY(x, y, n)
+
+    b1 = ((somatoria_xy - (n * xAvg * yAvg))/(xQuad - (n * (xAvg**2))))
+
+    return b1
+#end-function
+
+def calcula_b0(b1, xAvg, yAvg):
+    return (yAvg - (b1*xAvg))
+#end-function
+
+def calcula_rxy(n, xTotal, yTotal, xQuad, yQuad,):
+    return((n*somatoria_xy) - (xTotal*yTotal))/(math.sqrt((n * xQuad - (math.pow(xTotal, 2))) * (n * yQuad - (math.pow(yTotal, 2)))))
+#end-function
+
+def calcula_rQuad(rxy):
+    return rxy**2
+#end-function
+
+def calcula_yk(b0, b1, xk = 386):
+   return b0 + (b1*xk)
+#end-function
+
+###################### CASO DE TESTE 1 ######################
 n = 10
-xk = 386
 x = [130, 650, 99, 150, 128, 302, 95, 945, 368, 961]
 y = [186, 699, 132, 272, 291, 331, 199, 1890, 788, 1601]
 
-b1, b0, rxy, r2, yk = calculaDados(x, y, n, xk)
-
-print('b1: ', b1)
-print('b0: ', b0)
-print('rxy: ', rxy)
-print('r2: ', r2)
-print('yk: ', yk)
-print()
-
-########################
-print()
-print('CASO DE TESTE 2')
-
+###################### CASO DE TESTE 2 ######################
+n = 10
+x = [130, 650, 99, 150, 128, 302, 95, 945, 368, 961]
 y = [15, 69.9, 6.5, 22.4, 28.4, 65.9, 19.4, 198.7, 38.8, 138.2]
 
-b1, b0, rxy, r2, yk = calculaDados(x, y, n, xk)
 
-print('b1: ', b1)
-print('b0: ', b0)
-print('rxy: ', rxy)
-print('r2: ', r2)
-print('yk: ', yk)
-
-########################
-print()
-print('CASO DE TESTE 3')
-
+###################### CASO DE TESTE 3 ######################
+n = 10
 x = [163, 765, 141, 166, 137, 355, 136, 1206, 433, 1130]
 y = [186, 699, 132, 272, 291, 331, 199, 1890, 788, 1601]
 
-b1, b0, rxy, r2, yk = calculaDados(x, y, n, xk)
-
-print('b1: ', b1)
-print('b0: ', b0)
-print('rxy: ', rxy)
-print('r2: ', r2)
-print('yk: ', yk)
-
-########################
-print()
-print('CASO DE TESTE 4')
-
+###################### CASO DE TESTE 4 ######################
+n = 10
 x = [163, 765,  141, 166,  137,  355,  136,  1206,  433,  1130]
 y = [15,  69.9, 6.5, 22.4, 28.4, 65.9, 19.4, 198.7, 38.8, 138.2]
 
-b1, b0, rxy, r2, yk = calculaDados(x, y, n, xk)
-
-print('b1: ', b1)
-print('b0: ', b0)
-print('rxy: ', rxy)
-print('r2: ', r2)
-print('yk: ', yk)
--- psp1/test.py
+++ psp1/test.py
@@ -1,13 +1,25 @@
-from regressaoLinear import calculaDados
+import regressaoLinear
 
-n = 10
+
 xk = 386
 x = [130, 650, 99, 150, 128, 302, 95, 945, 368, 961]
 y = [186, 699, 132, 272, 291, 331, 199, 1890, 788, 1601]
 
 def test_b1():
-    dados = calculaDados(x, y, n, xk)
-    b1 = dados[0]
+    n = 10
+    b1 = regressaoLinear.calcula_b1(x, y, n)
     b1 = round(b1, 6)
     assert b1 == 1.727932
 #end-function
+
+def test_b1_n0():
+    n = 0
+    b1 = regressaoLinear.calcula_b1(x, y, n)
+    assert b1 == None
+#end-function
+
+# def test_n():
+#     n = 0
+#     dados = calculaDados(x, y, n, xk)
+#     assert dados == False
+# #end-function
