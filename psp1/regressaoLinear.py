0e070d4f5276c8dc8cb8bbb141677ce6fa0ece08
alescrocaro <alescrocaro@gmail.com>
2022-09-15 20:15:15 -0300

    testes finalizados

--- psp1/regressaoLinear.py
+++ psp1/regressaoLinear.py
@@ -23,11 +23,6 @@ def calcula_yTotal(y, n):
 #end-function
 
 def calcula_somatoriaXY(x, y, n):
-    # if (n <= 0): return None
-    # if (len(x) != len(y)): return None
-    # if (len(x) != n): return None
-    # if (len(y) != n): return None
-
     soma = 0
     for i in range(n):
         soma = soma + x[i]*y[i]
@@ -59,8 +54,10 @@ def calcula_yAvg(yTotal, n):
     return yTotal/n
 #end-function
 
-def calcula_b1(x, y, n):
+def calcula_yk(x, y, n,  xk = 386):
     if(n <= 0): return None
+    if (len(x) != len(y)): return None
+    if (len(x) != n): return None
 
     xTotal = calcula_xTotal(x, n)
     xAvg = calcula_xAvg(xTotal, n)
@@ -69,49 +66,33 @@ def calcula_b1(x, y, n):
     yAvg = calcula_yAvg(yTotal, n)
 
     xQuad = calcula_xQuad(x, n)
+
+    somatoria_xy = calcula_somatoriaXY(x, y, n)
+
+    b1 = (somatoria_xy - (n * xAvg * yAvg))/(xQuad - (n * (xAvg**2)))
+    b0 = yAvg - (b1*xAvg)
+    yk = b0 + (b1*xk)
+
+    return yk, b0, b1
+#end-function
+
+def calcula_r(x, y, n):
+    if(n <= 0): return None
+    if (len(x) != len(y)): return None
+    if (len(x) != n): return None
+    if (len(y) != n): return None
+
+    xTotal = calcula_xTotal(x, n)
+    yTotal = calcula_yTotal(y, n)
+
+    xQuad = calcula_xQuad(x, n)
     yQuad = calcula_yQuad(y, n)
 
     somatoria_xy = calcula_somatoriaXY(x, y, n)
 
-    b1 = ((somatoria_xy - (n * xAvg * yAvg))/(xQuad - (n * (xAvg**2))))
-
-    return b1
+    
+    rxy = ((n*somatoria_xy) - (xTotal*yTotal))/(math.sqrt((n * xQuad - (math.pow(xTotal, 2))) * (n * yQuad - (math.pow(yTotal, 2)))))
+    r2 = rxy**2
+    
+    return rxy, r2
 #end-function
-
-def calcula_b0(b1, xAvg, yAvg):
-    return (yAvg - (b1*xAvg))
-#end-function
-
-def calcula_rxy(n, xTotal, yTotal, xQuad, yQuad,):
-    return((n*somatoria_xy) - (xTotal*yTotal))/(math.sqrt((n * xQuad - (math.pow(xTotal, 2))) * (n * yQuad - (math.pow(yTotal, 2)))))
-#end-function
-
-def calcula_rQuad(rxy):
-    return rxy**2
-#end-function
-
-def calcula_yk(b0, b1, xk = 386):
-   return b0 + (b1*xk)
-#end-function
-
-###################### CASO DE TESTE 1 ######################
-n = 10
-x = [130, 650, 99, 150, 128, 302, 95, 945, 368, 961]
-y = [186, 699, 132, 272, 291, 331, 199, 1890, 788, 1601]
-
-###################### CASO DE TESTE 2 ######################
-n = 10
-x = [130, 650, 99, 150, 128, 302, 95, 945, 368, 961]
-y = [15, 69.9, 6.5, 22.4, 28.4, 65.9, 19.4, 198.7, 38.8, 138.2]
-
-
-###################### CASO DE TESTE 3 ######################
-n = 10
-x = [163, 765, 141, 166, 137, 355, 136, 1206, 433, 1130]
-y = [186, 699, 132, 272, 291, 331, 199, 1890, 788, 1601]
-
-###################### CASO DE TESTE 4 ######################
-n = 10
-x = [163, 765,  141, 166,  137,  355,  136,  1206,  433,  1130]
-y = [15,  69.9, 6.5, 22.4, 28.4, 65.9, 19.4, 198.7, 38.8, 138.2]
-
--- psp1/test.py
+++ psp1/test.py
@@ -1,21 +1,91 @@
 import regressaoLinear
 
 
+# ###################### CASO DE TESTE 1 ######################
+
 xk = 386
 x = [130, 650, 99, 150, 128, 302, 95, 945, 368, 961]
 y = [186, 699, 132, 272, 291, 331, 199, 1890, 788, 1601]
 
-def test_b1():
+# ###################### CASO DE TESTE 2 ######################
+# n = 10
+# x = [130, 650, 99, 150, 128, 302, 95, 945, 368, 961]
+# y = [15, 69.9, 6.5, 22.4, 28.4, 65.9, 19.4, 198.7, 38.8, 138.2]
+
+
+# ###################### CASO DE TESTE 3 ######################
+# n = 10
+# x = [163, 765, 141, 166, 137, 355, 136, 1206, 433, 1130]
+# y = [186, 699, 132, 272, 291, 331, 199, 1890, 788, 1601]
+
+# ###################### CASO DE TESTE 4 ######################
+# n = 10
+# x = [163, 765,  141, 166,  137,  355,  136,  1206,  433,  1130]
+# y = [15,  69.9, 6.5, 22.4, 28.4, 65.9, 19.4, 198.7, 38.8, 138.2]
+
+
+def test_b0_dadosCertos():
     n = 10
-    b1 = regressaoLinear.calcula_b1(x, y, n)
+    data = regressaoLinear.calcula_yk(x, y, n)
+    b0 = data[1] 
+    b0 = round(b0, 4)
+    assert b0 == -22.5525
+#end-function
+
+def test_b1_dadosCertos():
+    n = 10
+    data = regressaoLinear.calcula_yk(x, y, n)
+    b1 = data[2] 
     b1 = round(b1, 6)
     assert b1 == 1.727932
 #end-function
 
-def test_b1_n0():
+def test_yk_dadosCertos():
+    n = 10
+    data = regressaoLinear.calcula_yk(x, y, n)
+    yk = data[0] 
+    yk = round(yk, 4)
+    assert yk == 644.4294
+#end-function
+
+def test_yk_nMenorTamXouY():
+    n = 9
+    data = regressaoLinear.calcula_yk(x, y, n)
+    assert data == None
+#end-function
+
+def test_yk_nIgual0():
     n = 0
-    b1 = regressaoLinear.calcula_b1(x, y, n)
-    assert b1 == None
+    data = regressaoLinear.calcula_yk(x, y, n)
+    assert data == None
+#end-function
+
+def test_yk_nMenor0():
+    n = -8
+    data = regressaoLinear.calcula_yk(x, y, n)
+    assert data == None
+#end-function
+
+def test_rxy_dadosCertos():
+    n = 10
+    data = regressaoLinear.calcula_r(x, y, n)
+    rxy = data[0] 
+    rxy = round(rxy, 4)
+    assert rxy == 0.9545
+#end-function
+
+def test_r2_dadosCertos():
+    n = 10
+    data = regressaoLinear.calcula_r(x, y, n)
+    r2 = data[1] 
+    r2 = round(r2, 4)
+    assert r2 == 0.9111
+#end-function
+
+def test_r2_nMenorTamXouY():
+    n = 9
+    data = regressaoLinear.calcula_r(x, y, n)
+    assert data == None
 #end-function
 
 # def test_n():
